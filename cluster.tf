d04b02fb4960fff6759610879850a04dddbe4ccc
unknown <kumaradarsh921@gmail.com>
2024-04-14 19:07:03 +0530

    Git Auto push

--- cluster.tf
+++ cluster.tf
@@ -13,39 +13,9 @@ resource "aws_elasticache_cluster" "redis" {
 
 resource "aws_elasticache_parameter_group" "redis" {
   name   = "roboshop-${var.ENV}-redis-pg"
-  family = "redis2.8"
+  family = "redis6.x"
 }
 
-# Creates DOCDB cluster
-# resource "aws_docdb_cluster" "docdb" {
-#   cluster_identifier      = "roboshop-${var.ENV}-docdb"
-#   engine                  = "docdb"
-#   master_username         = "admin1"
-#   master_password         = "roboshop1"
-#   skip_final_snapshot     = true            # value will be false in production. In lab, we will be using true
-#   db_subnet_group_name    = aws_docdb_subnet_group.docdb.name
-#   vpc_security_group_ids  = [aws_security_group.allows_docdb.id]
-# }
-# # Creates DocDB instances and adds to the cluster
-
-# resource "aws_docdb_cluster_instance" "cluster_instances" {
-#   count              = 1
-#   identifier         = "roboshop-${var.ENV}-docdb"
-#   cluster_identifier = aws_docdb_cluster.docdb.id
-#   instance_class     = "db.t3.medium"
-# }
-
-
-# # Create DocDB subnet group
-
-# resource "aws_docdb_subnet_group" "docdb" {
-#   name       = "roboshop-${var.ENV}-docdb-subnet-group"
-#   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
-
-#   tags = {
-#     Name = "roboshop-${var.ENV}-docdb-subnet-group"
-#   }
-# }
 
 resource "aws_elasticache_subnet_group" "redis" {
   name       = "roboshop-${var.ENV}-redis-subnet-group"
