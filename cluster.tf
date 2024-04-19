b7360954c50523015a704c32e089834e06891a31
unknown <kumaradarsh921@gmail.com>
2024-04-19 21:19:24 +0530

    Git Auto push

--- cluster.tf
+++ cluster.tf
@@ -3,7 +3,7 @@ resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "roboshop-${var.ENV}-redis"
   engine               = "redis"
   node_type            = var.REDIS_INSTANCE_TYPE
-  num_cache_nodes      = 1
+  num_cache_nodes      = var.REDIS_INSTANCE_COUNT
   parameter_group_name = aws_elasticache_parameter_group.redis.name
   subnet_group_name    = aws_elasticache_subnet_group.redis.name
   security_group_ids   = [aws_security_group.allows_redis.id]
--- variables.tf
+++ variables.tf
@@ -2,3 +2,4 @@ variable "ENV" {}
 variable "REDIS_ENGINE_VERSION" {}
 variable "REDIS_INSTANCE_TYPE" {}
 variable "REDIS_PORT" {}
+variable "REDIS_INSTANCE_COUNT" {}
